41b76b80ce3aa7fb78ab09318cbe8d9e4001ab35
Alice Martin <kadoulys@hotmail.com>
2019-05-24 16:04:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 /opt/gitlab/embedded/bin/runsvdir-start&
-gitlab-ctl reconfigure && service ssh start
+gitlab-ctl reconfigure && service ssh start && export GIT_SSL_NO_VERIFY=true
 tail -f /dev/null
 
