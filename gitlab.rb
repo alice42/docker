a2cf50627404d0c72b49912791085a132663b8ff
Alice Martin <kadoulys@hotmail.com>
2019-05-31 10:55:55 +0200

    Update gitlab.rb

--- gitlab.rb
+++ gitlab.rb
@@ -1,4 +1,4 @@
-external_url "https://192.168.99.104:8443"
-nginx['ssl_certificate'] = "/etc/gitlab/ssl/192.168.99.104.crt"
-nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/192.168.99.104.key"
+external_url "https://${var}"
+nginx['ssl_certificate'] = "/etc/gitlab/ssl/${var}.crt"
+nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/${var}key"
 gitlab_rails['gitlab_shell_ssh_port'] = 8022
